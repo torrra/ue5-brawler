49b7e8ebf8647908044cd451455d1c73ba3edd40
Noah DE PISCHOF <n.depischof@student.isartdigital.com>
2024-11-07 10:54:42 -0500

    Add Enemy AI controller and behavior tree
    Add GladiatorAIController c++ class and AIC_Gladiator blueprint
    to run BT_GladiatorEnemy

---
+++ Source/GladiatorGame/GladiatorAIController.cpp
@@ -0,0 +1,85 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GladiatorAIController.h"
+#include "Gladiator.h"
+#include "LifeComponent.h"
+
+
+#include <BehaviorTree/BehaviorTreeComponent.h>
+#include <BehaviorTree/BehaviorTree.h>
+#include <BehaviorTree/BlackboardComponent.h>
+#include <Components/CapsuleComponent.h>
+#include <Kismet/GameplayStatics.h>
+#include <Navigation/CrowdFollowingComponent.h>
+
+
+AGladiatorAIController::AGladiatorAIController(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer.SetDefaultSubobjectClass<UCrowdFollowingComponent>(TEXT("PathFollowingComponent")))
+{
+}
+
+void AGladiatorAIController::SetEnemyTargetLocation(FVector2D Target)
+{
+	TargetLocation.X = Target.X;
+	TargetLocation.Z = GetPawn()->GetActorLocation().Z;
+	TargetLocation.Y = Target.Y;
+
+	if (!IsCloseToTarget())
+	{
+		Blackboard->SetValueAsVector(TEXT("TargetPosition"), TargetLocation);
+		GetBlackboardComponent()->SetValueAsBool(TEXT("TargetReached"), false);
+	}
+}
+
+void AGladiatorAIController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	TArray<AActor*>		GladiatorArray;
+
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), AGladiator::StaticClass(), GladiatorArray);
+	Blackboard->SetValueAsObject(TEXT("TargetActor"), UGameplayStatics::GetPlayerCharacter(GetWorld(), 0));
+
+	if (AGladiator* Gladiator = Cast<AGladiator>(GetCharacter()))
+		Gladiator->GetLifeComponent()->OnDeath.AddDynamic(this, &AGladiatorAIController::OnDeath);
+}
+
+void AGladiatorAIController::OnPossess(APawn* InPawn)
+{
+	Super::OnPossess(InPawn);
+
+	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, TEXT("AGladiatorAIController::OnPossess()"));
+
+	RunBehaviorTree(BehaviorTreeAsset);
+
+	// Populate blackboard
+	Blackboard->SetValueAsFloat(TEXT("IdealDistance"), IdealDistanceToTarget);
+	Blackboard->SetValueAsFloat(TEXT("ChaseSpeed"), ChaseSpeed);
+	Blackboard->SetValueAsFloat(TEXT("OnGuardWalkSpeed"), OnGuardWalkSpeed);
+}
+
+void AGladiatorAIController::OnDeath()
+{
+	BrainComponent->StopLogic(TEXT("Enemy is dead"));
+	bIsDead = true;
+}
+
+bool AGladiatorAIController::IsTargetPositionValid(const FVector2D& Target) const
+{
+	FVector2D	Location2D{ TargetLocation.X, TargetLocation.Y };
+	float		RadiusSquared = GetCharacter()->GetCapsuleComponent()->GetScaledCapsuleRadius() * 2.f;
+
+	RadiusSquared *= RadiusSquared;
+	return (Location2D - Target).SizeSquared() > RadiusSquared;
+}
+
+bool AGladiatorAIController::IsCloseToTarget() const
+{
+	return FVector::Distance(GetCharacter()->GetActorLocation(), TargetLocation) < AcceptableDistanceError;
+}
+
+bool AGladiatorAIController::IsDead() const
+{
+	return bIsDead;
+}
---
+++ Source/GladiatorGame/GladiatorAIController.h
@@ -0,0 +1,69 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "GladiatorAIController.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class GLADIATORGAME_API AGladiatorAIController : public AAIController
+{
+	GENERATED_BODY()
+
+public:
+
+	AGladiatorAIController(const FObjectInitializer& ObjectInitializer);
+
+	void SetEnemyTargetLocation(FVector2D Target);
+
+	bool IsTargetPositionValid(const FVector2D& Target) const;
+
+	bool IsCloseToTarget() const;
+
+	bool IsDead() const;
+
+protected:
+
+	void BeginPlay() override;
+
+	void OnPossess(APawn* InPawn) override;
+
+
+private:
+
+	UFUNCTION()
+	void OnDeath();
+
+	FVector	 TargetLocation;
+
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Behavior", meta = (AllowPrivateAccess = "true"));
+	TObjectPtr<class UBehaviorTree>			BehaviorTreeAsset;
+
+
+protected:
+
+	UPROPERTY(EditAnywhere, Category = "Behavior", meta = (AllowPrivateAccess = "true"))
+	float							IdealDistanceToTarget;
+
+
+	UPROPERTY(EditAnywhere, Category = "Behavior", meta = (AllowPrivateAccess = "true"))
+	float							AcceptableDistanceError;
+
+
+	UPROPERTY(EditAnywhere, Category = "Behavior", meta = (AllowPrivateAccess = "true"))
+	float							ChaseSpeed;
+
+	UPROPERTY(EditAnywhere, Category = "Behavior", meta = (AllowPrivateAccess = "true"))
+	float							OnGuardWalkSpeed;
+
+
+private:
+
+	bool							bIsDead = false;
+
+};
