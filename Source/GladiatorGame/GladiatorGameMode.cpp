493b57bbcedd07b85e92bcf9057097a8b124c716
Noah DE PISCHOF <n.depischof@student.isartdigital.com>
2024-11-12 14:11:14 -0500

    Add HUD_Player as default HUD
    Edit GladiatorGameMode.cpp to set HUD_Player class as default
    HUD class

--- Source/GladiatorGame/GladiatorGameMode.cpp
+++ Source/GladiatorGame/GladiatorGameMode.cpp
@@ -2,20 +2,22 @@
 
 
 #include "GladiatorGameMode.h"
+#include "GladiatorHUD.h"
 
 AGladiatorGameMode::AGladiatorGameMode()
 {
 	static ConstructorHelpers::FClassFinder<APawn> PlayerPawn(TEXT("/Game/Blueprints/Player/BP_Gladiator"));
 
-	if (PlayerPawn.Class != NULL)
-	{
+	if (NULL != PlayerPawn.Class)
 		DefaultPawnClass = PlayerPawn.Class;
-	}
 
 	static ConstructorHelpers::FClassFinder<APlayerController> PlayerController(TEXT("/Game/Blueprints/Player/PC_Gladiator"));
 
-	if (PlayerController.Class != NULL)
-	{
+	if (NULL != PlayerController.Class)
 		PlayerControllerClass = PlayerController.Class;
-	}
+
+	static ConstructorHelpers::FClassFinder<AHUD> Hud(TEXT("/Game/Blueprints/Player/HUD_Gladiator"));
+
+	if (NULL != Hud.Class)
+		HUDClass = Hud.Class;
 }
